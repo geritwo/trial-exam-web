d5b4bf7fb480041bf30fc4792872e1703198a327
GergoV <geritwo@gmail.com>
2017-01-08 23:35:56 +0100

    Build done; structure done

---
+++ app.js
@@ -0,0 +1,8 @@
+// Wrote loading while request pending
+
+var Caesars = function () {}
+
+var postTextToDecypher = function () {
+  var xhr = new XMLHttpRequest;
+  var 
+}
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+// CC endpoints
+
+// POST /decode
+// object in, out
+// a-zA-Z]
+
+// GET /decode/all
+
+// DB
+// separated decryption file
