e15cd1d9d3c5dd0371bfe97ae194e10a3b509120
GergoV <geritwo@gmail.com>
2017-01-09 01:38:20 +0100

    1 endpoint, client parsing and JC added. CORS also added. Pretty slight idea of what doing.

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 // Wrote loading while request pending
 
-var Caesars = function () {}
+var counter = 0;
 
-var postTextToDecypher = function () {
-  var xhr = new XMLHttpRequest;
-  var 
+var form = document.querySelector('form');
+var button = form.submit;
+button.addEventListener('click', postTextToDecypher);
+
+function postTextToDecypher () {
+  var text = form.text.value;
+  var shift = form.shift.value;
+
+  var requestObj = {
+    "shift": shift,
+    "text": text
+  }
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:3000/decode', true);
+
+  xhr.setRequestHeader("Content-type", "application/json");
+
+  xhr.send(JSON.stringify(requestObj));
+    xhr.onreadystatechange = getDeciphered;
+
+    function getDeciphered(rsp) {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+
+        console.log(JSON.parse(xhr.response))
+        toggleJC(); // NOTE: for debug
+      };
+    };
+  };
+
+
+// Toggle JC image
+var jcHimSelf = document.getElementById('jc');
+jcHimSelf.addEventListener('click', toggleJC);
+
+var jcImages = {
+  0 : "./images/JC_thumbsup.png",
+  1 : "./images/JC_thumbsdown.png"
 }
+
+function toggleJC() {
+  counter++;
+  var imgID = counter % 2;
+  console.log(imgID)
+  jcHimSelf.src = jcImages[imgID];
+  return counter;
+};
--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,33 @@
 
 // DB
 // separated decryption file
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var decipher = require('./decypher')
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/decode', function (req, res) {
+  var shift = req.body.shift;
+  var text = req.body.text;
+  var deciphered = decipher.caesarShift(text, shift);
+  var response = {
+    "status": "ok",
+    "text": deciphered
+  }
+  res.send(JSON.stringify(response));
+  res.sendstatus(200);
+});
+
+console.log('Server is up and running on localhost:3000')
+app.listen(3000);
